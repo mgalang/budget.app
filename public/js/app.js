856f851e7844257ea50022f189f50affd61cbdae
Marc Galang <marchenrygalang@gmail.com>
2013-04-17 23:52:54 +0300

    Added Credit controllers and models

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,58 @@
 (function(Spine, $, exports){
-  var Entry = Spine.Model.sub();
-  Entry.configure("Entry", "title");
-  Entry.extend(Spine.Model.Ajax);
+  var Credit = Spine.Model.sub();
+  Credit.configure("Credit", "title", "entryid", "value")
+    .extend(Spine.Model.Ajax)
+    .extend({
+      url: "/credits"
+    });
 
-  Entry.extend({
-    url: "/entries"
+  var Entry = Spine.Model.sub();
+  Entry.configure("Entry", "title")
+    .extend(Spine.Model.Ajax)
+    .extend({
+      url: "/entries"
+    });
+
+
+  var CreditItem = Spine.Controller.sub({
+    init: function(){
+      this.credit.bind('update', this.proxy(this.render));
+      this.credit.bind('destroy', this.proxy(this.remove));
+    },
+    render: function(){
+      var template = this.template();
+      this.replace(template(this.credit));
+      return this;
+    },
+    template: function(){
+      return Handlebars.compile($('#credit-list-tpl').html());
+    },
+    remove: function(){
+      this.el.remove();
+      this.release();
+    }
+  });
+ 
+  var Credits = Spine.Controller.sub({
+    el: $('#credits-list'),
+    init: function(){
+      Credit.bind('update', this.proxy(this.get));
+      this.get();
+    },
+    get: function(){
+      this.$('tbody').empty();
+      var _credits = Credit.select(this.proxy(function(credit){
+        return credit.entryid === this.id;
+      }));
+
+      for(var i in _credits){
+        this.add(_credits[i]);
+      }
+    },
+    add: function(credit){
+      var item = new CreditItem({ credit: credit });
+      this.$('tbody').append(item.render().el);
+    }
   });
 
   var EntryItem = Spine.Controller.sub({
@@ -34,7 +82,6 @@
     init: function(){
       Entry.bind('create', this.proxy(this.add));
       Entry.bind('refresh', this.proxy(this.refresh));
-      Entry.fetch();
     },
     add: function(item){
       var entry = new EntryItem({ entry: item });
@@ -42,12 +89,6 @@
     },
     refresh: function(){
       Entry.each(this.proxy(this.add));
-    },
-    delete: function(e){
-      e.preventDefault();
-
-      var id = $(e.currentTarget).data('id');
-      Entry.destroy(id);
     },
     submit: function(e){
       e.preventDefault();
@@ -60,28 +101,48 @@
 
   var EntryDetail = Spine.Controller.sub({
     el: $('#entry-detail'),
+    elements: {
+      '.title': 'title',
+      '.delete': 'delete_button',
+      '#entryid': 'entryid'
+    },
     init: function(){
-      this.el.html('No budget selected');
+      this.el.addClass('is-empty');
     },
     show: function(id){
+      this.el.removeClass('is-empty');
       if(Entry.exists(id)){
         var entry = Entry.find(id);
-        this.render(entry);
+
+        this.entryid.val(id);
+        this.title.html(entry.title);
+        this.delete_button.attr('href', '#/delete/'+entry.id);
+
+        new Credits({ id: entry.id });
       } else {
-        this.el.html('Budget not found');
+        this.init();
       }
+    }
+  });
+
+  var CreditForm = Spine.Controller.sub({
+    el: $('#credit-form'),
+    init: function(){
+      this.el.bind('submit', this.proxy(this.submit));
     },
-    render: function(view){
-      var template = this.template();
-      this.el.html(template(view));
-    },
-    template: function(){
-      return Handlebars.compile($('#entry-detail-tpl').html());
+    submit: function(e){
+      e.preventDefault();
+      
+      var credit = Credit.fromForm(e.target);
+      credit.save();
+
+      this.$('input[type=text]').val('');
     }
   });
 
   var entries = new Entries();
   var entry_detail = new EntryDetail();
+  var credit_form = new CreditForm();
 
   var App = Spine.Controller.sub({
     el: $('#app'),
@@ -89,8 +150,18 @@
       this.routes({
         "/entry/:id": function(params){
           entry_detail.show(params.id);
+        },
+        "/delete/:id": function(params){
+          Entry.destroy(params.id);
+          entry_detail.init();
+        },
+        "/delete/credit/:id": function(params){
+          Credit.destroy(params.id);
         }
       });
+
+      Entry.fetch();
+      Credit.fetch();
     }
   });
 
@@ -99,4 +170,5 @@
   Spine.Route.setup();
 
   exports.Entry = Entry;
+  exports.Credit= Credit;
 })(Spine, Spine.$, window);
