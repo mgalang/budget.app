e9d60a86590dffc51df6d262469fd58ab1e6de1e
Marc Galang <marchenrygalang@gmail.com>
2013-04-25 22:38:52 +0300

    Remove extra stuff generated by scaffold

--- app/models/debit.js
+++ app/models/debit.js
@@ -6,41 +6,7 @@ var Debit = function () {
     value: {type: 'number'}
   });
 
-  /*
-  this.property('login', 'string', {required: true});
-  this.property('password', 'string', {required: true});
-  this.property('lastName', 'string');
-  this.property('firstName', 'string');
-
-  this.validatesPresent('login');
-  this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
-  this.validatesLength('login', {min: 3});
-  // Use with the name of the other parameter to compare with
-  this.validatesConfirmed('password', 'confirmPassword');
-  // Use with any function that returns a Boolean
-  this.validatesWithFunction('password', function (s) {
-      return s.length > 0;
-  });
-
-  // Can define methods for instances like this
-  this.someMethod = function () {
-    // Do some stuff
-  };
-  */
-
 };
-
-/*
-// Can also define them on the prototype
-Debit.prototype.someOtherMethod = function () {
-  // Do some other stuff
-};
-// Can also define static methods and properties
-Debit.someStaticMethod = function () {
-  // Do some other stuff
-};
-Debit.someStaticProperty = 'YYZ';
-*/
 
 Debit = geddy.model.register('Debit', Debit);
 
